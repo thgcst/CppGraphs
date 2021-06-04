54d88bd09cd7ee3e6ea5961019a8226e3f0175e8
Matheus Lomba <mathlomba@gmail.com>
2021-06-04 18:25:48 -0300

    estudo de caso realizado

--- main.cpp
+++ main.cpp
@@ -8,11 +8,18 @@
 
 using namespace std;
 
-int main() {
-    weightedVector graph("Graphs/grafo_W_3.txt");
+int main()
+{
+    weightedVector graph("Graphs/rede_colaboracao.txt");
     clock_t start = clock();
 
-    cout << graph.eccentricity(1) << endl;
+    graph.shortestPath(2722,true,343930);
+    //cout << graph.MST(1, true) << endl;
+    /*for (int n = 0; n <= 100; n++)
+    {
+        cout << graph.eccentricity(n) << endl;
+    }*/
+    //cout << graph.eccentricity(10) << endl;
 
     clock_t end = clock();
     cout << (double)(end - start) / CLOCKS_PER_SEC << " segundos" << endl;
